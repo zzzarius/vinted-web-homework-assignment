fcd64c478db9634ddcaddaf597dfbf5556304083
Darius Zivertas <2293236+zzzarius@users.noreply.github.com>
2025-02-24 17:10:50 +0200

    usePageParam hook

---
+++ src/components/usePageParam.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+
+export function usePageParam() {
+    const [page, setPage] = useState<number>(0);
+
+    useEffect(() => {
+        const searchParams = new URL(window.location.href).searchParams;
+        const page = parseInt(searchParams.get("page") || "1", 10);
+        if (Number.isNaN(page)) {
+            setPage(1);
+            history.replaceState({ page }, "", new URL(`?page=${page}`, location.href));
+            return;
+        }
+        setPage(page);
+    }, []);
+
+    function nextPage() {
+        setPage((prev) => prev + 1);
+    }
+
+    function prevPage() {
+        setPage((prev) => prev > 0 ? prev - 1 : 1);
+    }
+
+    return { page, nextPage, prevPage };
+}
