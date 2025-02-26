9abf55d241b2533b60d22131aca5a56b4318a1da
Darius Zivertas <2293236+zzzarius@users.noreply.github.com>
2025-02-26 09:27:13 +0200

    Extract more values into custom properties

--- src/components/Card/PreviewDialog/PreviewDialog.ui.tsx
+++ src/components/Card/PreviewDialog/PreviewDialog.ui.tsx
@@ -5,7 +5,7 @@ import { Photo } from "../../../api/Pexels.types";
 
 interface PreviewDialogProps {
     isOpen: boolean
-    setIsOpen:  React.Dispatch<React.SetStateAction<boolean>>
+    setIsOpen: React.Dispatch<React.SetStateAction<boolean>>
     photo: Photo;
 }
 
@@ -28,7 +28,7 @@ export function PreviewDialog({ isOpen, setIsOpen, photo }: PreviewDialogProps) 
     return (
         <dialog className={styles.previewDialog} ref={ref}>
             <div className={styles.controls}>
-            <button aria-label="Close" className={styles.closeButton} onClick={handleClose} />
+                <button aria-label="Close" className={styles.closeButton} onClick={handleClose} />
             </div>
             <img className={styles.image} src={photo.src.large2x} alt={photo.alt} />
         </dialog>
